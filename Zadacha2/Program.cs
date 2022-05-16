f89e2109b063c792ad38c52e58223bd6bdb0a8ac
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-16 10:48:10 +0300

    Added Zadacha 2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int sum = 0;
+for (int i = 1; i <= a; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine($"Сумма числел от 1 до {a} = {sum}");
