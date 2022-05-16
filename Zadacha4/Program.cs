a86c1986eedb79460fa1c821963c2cdfae6d97f6
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-16 12:10:44 +0300

    Modfied p type for Zadacha 4

--- Zadacha4/Program.cs
+++ Zadacha4/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Написать программу вычисления произведения чисел от 1 до N
 Console.Write("Введите число n: ");
 int n = int.Parse(Console.ReadLine() ?? "0");
-int p = 1;
+double p = 1;
 for (int i = 1; i <= n; i++)
 {
     p = p * i;
