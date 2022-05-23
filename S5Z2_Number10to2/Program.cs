497e769981946b7f5fbf1b2af47f68bfcc5f8252
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-23 13:25:07 +0300

    Added tasks for Seminar 5

---
+++ S5Z2_Number10to2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+Console.Clear();
+Console.Write("Введите десятичное число ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+string result = "";
+string RevResult = "";
+while (num > 0)
+{
+    result = result + (num % 2).ToString();
+    num = num / 2;
+}
+int length = result.Length;
+for (int i = length - 1; i >=0; i--)
+    RevResult += result[i]; 
+Console.WriteLine($"{RevResult}");
+
+// // Второй вариант
+
+// Console.Write("Введите число: ");
+// int Number = int.Parse(Console.ReadLine() ?? "0");
+// int Bit = Number % 2;
+// int NewNumber = Bit;
+// Number = Number / 2;
+// int i = 1;
+// while (Number > 0)
+// {
+//     Bit = Number % 2;
+//     Number = Number / 2;
+//     NewNumber = NewNumber + Bit * (int)Math.Pow(10,i);
+//     i++;
+// }
+// Console.WriteLine(NewNumber);
---
+++ S5Z3_FibonachiLowerThanN/Program.cs
@@ -0,0 +1,32 @@
+﻿// Показать числа Фибоначчи меньше заданного числа N
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()?? "");
+
+int firstValue  = 0;
+int secondValue = 1;
+int result      = 0;
+Console.Write($"{firstValue} {secondValue} ");
+
+while(firstValue + secondValue < N)
+{
+    result = firstValue + secondValue;
+    Console.Write(result + " ");
+    firstValue = secondValue;
+    secondValue = result;
+}
+
+// //Показать числа Фибоначчи меньше заданного числа N (по кол-ву)
+
+// Console.WriteLine("Введите число N: ");
+// int n = int.Parse(Console.ReadLine());
+// int a = 1;
+// int b = 0;
+// int result;
+// for (int i =0;i < n;i++)
+// {
+//     result = a + b;
+//     Console.WriteLine(result + " ");
+//     a = b;
+//     b = result;
+// }
