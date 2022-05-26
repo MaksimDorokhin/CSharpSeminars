cf139ec96e5615d8e1ffc90c87a0b04e2611e5ed
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-26 11:07:24 +0300

    Added S6Z2 task: 2D array even to oposite

--- S6Z1_Print2DArray/Program.cs
+++ S6Z1_Print2DArray/Program.cs
@@ -14,3 +14,36 @@ for (int i = 0; i < m; i++)
     }
     Console.WriteLine();
 }
+
+// // Показать двумерный массив размером m×n заполненный целыми числами
+// void PrintArray(int[,] matr)
+// {
+//     for (int i =0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             Console.Write($"{matr[i, j]} ");
+//         }
+//     Console.WriteLine();    
+//     }
+// } 
+
+// void FillArray(int[,] matr)
+// {
+//     for (int i =0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i,j] = new Random().Next(1,10); 
+//         }  
+//     }
+// } 
+// Console.WriteLine("Введите число m, n: ");
+// int m = int.Parse(Console.ReadLine()??"0");
+// int n = int.Parse(Console.ReadLine()??"0");
+
+// int[,] matrix = new int [m,n];
+
+// FillArray(matrix);
+// Console.WriteLine();
+// PrintArray(matrix);
---
+++ S6Z2_2DArrayChangeToOpposite/Program.cs
@@ -0,0 +1,46 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+void PrintArray(int[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();    
+    }
+} 
+void FillArray(int[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10); 
+        }  
+    }
+} 
+void EvenToOposite(int[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (matr[i,j] % 2 == 0) matr[i,j] = - matr[i,j];
+        }  
+    }
+}
+Console.Clear();
+Console.WriteLine("Введите кол-во строк: ");
+int n = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Введите кол-во столбцов: ");
+int k = int.Parse(Console.ReadLine()??"0");
+
+int[,] matrix = new int [n,k];
+
+FillArray(matrix);
+Console.WriteLine("Начальный массив: ");
+PrintArray(matrix);
+EvenToOposite(matrix);
+Console.WriteLine("Массив после замены нечетных элементов на противоположные: ");
+PrintArray(matrix);
