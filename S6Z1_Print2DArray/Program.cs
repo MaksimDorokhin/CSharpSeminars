fad5af1fb3e83d13035ed789c380654cd2e907bf
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-26 10:33:31 +0300

    Added S6Z1 task: input and print 2D array

---
+++ S6Z1_Print2DArray/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+Console.Clear();
+Console.Write("Введите кол-во строк двухмерного массива: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите кол-во столбцов двухмерного массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[,] arr = new int[m,n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        arr[i,j] = new Random().Next(-10,11);
+        Console.Write($"{arr[i,j]} ");
+    }
+    Console.WriteLine();
+}
