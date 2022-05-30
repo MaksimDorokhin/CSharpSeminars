c7dbdb37a88e6523a3e87735413c1ca9f35b8108
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-30 12:01:00 +0300

    Added S7Z2 task: Mean arifmetic of columns in 2D array

---
+++ S7Z2_MeanArifm/Program.cs
@@ -0,0 +1,40 @@
+﻿// Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+void FillArray(int[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10); 
+        }  
+    }
+} 
+void PrintArray(int[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();    
+    }
+} 
+double MeanAriphm(int[,] matr, int column)
+{
+    double sum = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+        sum = sum + matr[i,column];
+    return sum / matr.GetLength(0);
+}
+Console.Clear();
+Console.WriteLine("Введите кол-во строк: ");
+int n = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Введите кол-во столбцов: ");
+int k = int.Parse(Console.ReadLine()??"0");
+int[,] matrix = new int [n,k];
+FillArray(matrix);
+Console.WriteLine("Полученный массив: ");
+PrintArray(matrix);
+for (int j = 0; j < matrix.GetLength(1); j++)
+    Console.WriteLine($"Среднее арифметическое столбца {j+1} = {MeanAriphm(matrix, j)}"); 
