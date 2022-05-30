1f11dfee768a3e9d6c9d54f7bfe93e9985fcc0d5
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-30 11:20:39 +0300

    Added S7Z1 task: Sum of main diagonal in square matrix

---
+++ S7Z1_SumsOfDiagonalIn2DArray/Program.cs
@@ -0,0 +1,40 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали
+void FillArray(int[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10); 
+        }  
+    }
+} 
+void PrintArray(int[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();    
+    }
+} 
+int SumOfDiagonal(int[,] matr)
+{
+    int sum = 0;
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        sum = sum + matr[i,i];
+    }
+    return sum;
+}
+Console.Clear();
+Console.Write("Введите кол-во строк и столбцов квадтраной матрицы ");
+int k = int.Parse(Console.ReadLine() ?? "0");
+int[,] square = new int[k,k];
+FillArray(square);
+Console.WriteLine("Такая получилась матрица:");
+PrintArray(square);
+int result = SumOfDiagonal(square);
+Console.WriteLine($"Сумма элементов главной диагонали: {result}");
