513503a45eabdda70a24e65549bc3d5cf8a42f1e
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-06-06 14:00:18 +0300

    Added S9Z2 task: Recursion from 1 to N

---
+++ S9Z2_RecursionFrom1ToN/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+
+// void PrintNumber(int n)
+// {
+//     n--;
+//     if(n == 0) return;
+//     PrintNumber(n);
+//     Console.Write(n + " ");
+// }
+
+// int N = 20;
+
+// PrintNumber(N);
+
+void PrintNumber(int n)
+{
+    if (n < 1) return;
+    PrintNumber(n - 1);
+    Console.Write(n + " ");
+}
+PrintNumber(10);
