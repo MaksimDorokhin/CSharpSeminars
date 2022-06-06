8f0edf7b80494dc4c655505af766ac02589be75a
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-06-06 10:54:41 +0300

    Added S9Z1 task: Print from N to 1 with recursion

---
+++ S9Z1_RecursionFromNto1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать натуральные числа от N до 1, N задано
+void PrintNumber(int n)
+{
+    Console.Write(n + " ");
+    n--;
+    if(n == 0) return;
+    PrintNumber(n);
+}
+
+int N = 20;
+
+PrintNumber(N);
