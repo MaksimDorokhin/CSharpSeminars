2e3974a81f758cd4a89084ff3e38e17081d4d8c4
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-06-09 11:50:18 +0300

    Added S10Z2 task: Recursion Pow

---
+++ S10Z2_RecursionPow/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+int RecPow (int a, int b) => b == 0? 1 : a * RecPow(a, b - 1);
+
+Console.Write("Введите число А: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите степень B: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Число {a} в степени {b} = {RecPow(a, b)}");
